5bfa5bd5ca9358607b0490e88b92e293cd16f6e1
Chang Mao Yang <108061433+Jeffreymaomao@users.noreply.github.com>
2022-10-18 05:18:31 +0800

    v1

---
+++ quiz/javascript.js
@@ -0,0 +1,19 @@
+'use strict';
+var L = document.querySelectorAll('.question > li');
+var N = document.querySelectorAll('.sidebar > div > a');
+var y1 = 0;
+var y2 = 0;
+var check = 0;
+window.addEventListener('scroll', (event) => {
+    var H = window.innerHeight;
+    for (var i = 0; i < L.length; i++){
+        y1 = L[i].getBoundingClientRect().top;
+        y2 = L[i].getBoundingClientRect().button;
+        check = (y1>0 & y1<H) | (y2>0 & y2<H) | (y1<0 & y2>H)
+        if(check){
+            N[i].style.backgroundColor = 'red';
+        }else{
+            N[i].style.backgroundColor = 'black';
+        }
+    }
+});
